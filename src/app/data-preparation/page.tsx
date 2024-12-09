3d8686ff0d4943e109b5311a0cece29eeb4f8a60
iJuanB <juantenorio2004@gmail.com>
2024-12-09 10:12:14 -0500

    initial commit

---
+++ src/app/data-preparation/page.tsx
@@ -0,0 +1,10 @@
+import DataPreparationForm from '@/components/DataPreparationForm'
+
+export default function DataPreparationPage() {
+  return (
+    <div className="min-h-screen bg-[#0F0F10] text-white flex items-center justify-center p-4">
+      <DataPreparationForm />
+    </div>
+  )
+}
+
